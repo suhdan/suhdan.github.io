f4167b17f6b7b740791d46c606f862bcd95e3d2e
Daniel Suh <dansuh3593@gmail.com>
2018-06-21 22:49:17 -0400

    Add css loaders and webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    hot: true,
+    quiet: true,
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ]
   }
-};
+}
